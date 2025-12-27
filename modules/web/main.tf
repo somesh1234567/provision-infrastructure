8354b75a17dc9f00cb9d8e7d5f9494589b37eedf
Somesh Panigrahi <sonupanigrahi28@gmail.com>
2025-12-27 17:21:46 +0530

    added a attribute

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "server" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  security_groups = [ var.security_group_id ]
+  vpc_security_group_ids = [var.security_group_id]
 
   tags = {
     Name = "demo-web-server"
