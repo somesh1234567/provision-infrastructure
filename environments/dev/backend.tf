c19d7da66deb199df25c393a13fa67bc517efcc3
Somesh Panigrahi <sonupanigrahi28@gmail.com>
2025-12-31 15:56:18 +0530

    changing the bucket name

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "somesh-bucket-3028"
+    bucket = "somesh-bucket-1999"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
